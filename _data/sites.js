6938acc19367866b76bf402331589ee28a96699d
ruinyourfun <versatevel.loic@gmail.com>
2020-07-22 09:53:52 +0200

    add subpages

--- _data/sites.js
+++ _data/sites.js
@@ -9,8 +9,8 @@ module.exports = {
 		urls: [
 			"https://cowboy.com/",
 			"https://www.vanmoof.com/",
-			"https://amplerbikes.com/",
 			"https://angell.bike/",
+			"https://amplerbikes.com/",
 			"https://www.trekbikes.com/",
 			"https://www.raleigh.co.uk/",
 		]
@@ -26,10 +26,17 @@ module.exports = {
 			"https://cowboy.com/",
 			"https://cowboy.com/pages/e-bike-app/",
 			"https://cowboy.com/pages/easy-rider/",
-			"https://cowboy.com/blogs/reviews/",
 			"https://cowboy.com/pages/business/",
+			"https://cowboy.com/blogs/reviews/",
+			"https://cowboy.com/blogs/stories/",
 			"https://cowboy.com/pages/test-ride/",
 			"https://cowboy.com/cart/",
-		]
+			"https://cowboy.com/pages/careers",
+			"https://cowboy.com/pages/contact-us",
+			"https://cowboy.com/pages/delivery",
+			"https://cowboy.com/pages/returns",
+			"https://cowboy.com/pages/warranty",
+			"https://cowboy.com/pages/downloads",
+			"https://cowboy.com/pages/tutorials"		]
 	}
 };
